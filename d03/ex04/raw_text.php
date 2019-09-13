f7442644d69f1f3e5711c72029708a12bf996570
rpeepz <r.papagna.42@gmail.com>
2019-09-12 23:20:06 -0700

    other small php

---
+++ d03/ex04/raw_text.php
@@ -0,0 +1,13 @@
+<?php  
+	header('Content-type: text/plain');
+?>
+<html>
+	<head>
+		<title>raw_text</title>
+		<style> body {background-color: rgb(130, 130, 190)}
+		</style>
+	</head>
+	<body>
+		<H1>Plain text testing on ex04</H1>
+	</body>
+</html>
---
+++ d03/ex05/read_img.php
@@ -0,0 +1,6 @@
+<?php
+
+    $file = "../img/42.png";
+    header('Content-type: image/png');
+    readfile($file);
+?>
